2ba0f6f59cdf6c1bcda92779cbf82af35d048f43
yasuyuky <yasuyuki.ymd@gmail.com>
2017-06-26 10:36:32 +0900

    Add (set|get|end)spent implementation

--- src/lib.rs
+++ src/lib.rs
@@ -204,13 +204,66 @@ pub extern "C" fn _nss_ghteam_getspnam_r(cnameptr: *const libc::c_char,
 }
 
 #[no_mangle]
-pub extern "C" fn _nss_ghteam_setspent() -> libc::c_int { libc::c_int::from(NssStatus::Success) }
+pub extern "C" fn _nss_ghteam_setspent() -> libc::c_int {
+    let pid = unsafe { libc::getpid() };
+    std::fs::create_dir_all(RUN_DIR).unwrap();
+    let mut idx_file:File = File::create(format!("{}/{}.index",RUN_DIR,pid)).unwrap();
+    idx_file.write(b"0").unwrap();
+    let mut list_file:File = File::create(format!("{}/{}.list",RUN_DIR,pid)).unwrap();
+    let (_,members) = CLIENT.get_team_members().unwrap();
+    for member in members.values () {
+        list_file.write(format!("{}\t{}\n",member.login,member.id).as_bytes()).unwrap();
+    }
+    libc::c_int::from(NssStatus::Success)
+}
 
 #[no_mangle]
-pub extern "C" fn _nss_ghteam_getspent_r() -> libc::c_int { libc::c_int::from(NssStatus::Unavail) }
+pub extern "C" fn _nss_ghteam_getspent_r(
+    spbuf: *mut Spwd,
+    buf: *mut libc::c_char,
+    buflen: libc::size_t,
+    spbufp: *mut *mut Spwd,
+    ) -> libc::c_int {
+    let pid = unsafe { libc::getpid() };
+    let mut idx_file:File =  OpenOptions::new().read(true).write(true).open(format!("{}/{}.index",RUN_DIR,pid)).unwrap();
+    let mut idx_string = String::new();
+    idx_file.read_to_string(&mut idx_string).unwrap();
+    let idx:usize = idx_string.parse().unwrap();
+    let list = BufReader::new(File::open(format!("{}/{}.list",RUN_DIR,pid)).unwrap());
+    for (i,line) in list.lines().enumerate() {
+        if i!=idx { continue }
+        if let Ok(l) = line {
+            let mut buffer = Buffer::new(buf,buflen);
+            let words:Vec<&str> = l.split("\t").collect();
+            match unsafe {(*spbuf).pack(
+                &mut buffer,
+                words[0],
+                "!!",
+                -1,-1,-1,-1,-1,-1,0
+            )} {
+                Ok(_) => {
+                    idx_file.seek(SeekFrom::Start(0)).unwrap();
+                    idx_file.write(format!("{}",idx+1).as_bytes()).unwrap();
+                    unsafe {*spbufp = spbuf};
+                    return libc::c_int::from(NssStatus::Success)
+                },
+                Err(_) => {
+                    unsafe {*spbufp = std::ptr::null_mut()};
+                    return nix::Errno::ERANGE as libc::c_int
+                }
+            }
+        }
+    }
+    libc::c_int::from(NssStatus::Unavail)
+}
 
 #[no_mangle]
-pub extern "C" fn _nss_ghteam_endspent() -> libc::c_int { libc::c_int::from(NssStatus::Success) }
+pub extern "C" fn _nss_ghteam_endspent() -> libc::c_int {
+    let pid = unsafe { libc::getpid() };
+    std::fs::remove_file(format!("{}/{}.index",RUN_DIR,pid)).unwrap_or(());
+    std::fs::remove_file(format!("{}/{}.list",RUN_DIR,pid)).unwrap_or(());
+    libc::c_int::from(NssStatus::Success)
+}
 
 #[no_mangle]
 pub extern "C" fn _nss_ghteam_getgrgid_r(gid: libc::gid_t,
