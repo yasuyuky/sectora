fc10c3093b52f9071ab34c250ecdcbef100944b9
yasuyuky <yasuyuki.ymd@gmail.com>
2017-08-18 21:44:40 +0900

    Add a subcommand to check the configuration file

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,7 @@ use statics::CLIENT;
 
 fn main() {
     let user_arg = Arg::with_name("USER").required(true).index(1).help("user name");
+    let conf_arg = Arg::with_name("CONF").required(true).index(1).help("conf path");
     let app = App::new(crate_name!())
         .version(crate_version!())
         .author(crate_authors!())
@@ -28,10 +29,17 @@ fn main() {
         .subcommand(SubCommand::with_name("key").about("Gets user public key").arg(user_arg))
         .subcommand(SubCommand::with_name("pam").about("Executes pam check"))
         .subcommand(SubCommand::with_name("cleanup").about("Cleans caches up"))
+        .subcommand(SubCommand::with_name("check").about("Check configuration").arg(conf_arg))
         .get_matches();
 
     match app.subcommand() {
         ("key", Some(sub)) => CLIENT.print_user_public_key(sub.value_of("USER").unwrap()).unwrap(),
+        ("check", Some(sub)) => {
+            match structs::Config::new(std::path::Path::new(sub.value_of("CONF").unwrap())) {
+                Ok(_) => std::process::exit(0),
+                Err(_) => std::process::exit(11),
+            }
+        }
         ("cleanup", Some(_)) => CLIENT.clear_all_caches().unwrap(),
         ("pam", Some(_)) => {
             match std::env::var("PAM_USER") {
