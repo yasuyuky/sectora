101e89d8abe808af65e55ce46f567009e311eaa2
yasuyuky <yasuyuki.ymd@gmail.com>
2018-11-11 00:44:23 +0900

    Mark public functions as unsafe

--- src/lib.rs
+++ src/lib.rs
@@ -64,7 +64,7 @@ macro_rules! succeed {
 
 macro_rules! fail {
     ($err_no_p:ident, $err_no:expr, $return_val:expr) => {{
-        unsafe { *$err_no_p = $err_no as libc::c_int };
+        *$err_no_p = $err_no as libc::c_int;
         return libc::c_int::from($return_val);
     }};
 }
@@ -80,7 +80,7 @@ macro_rules! get_or_again {
         match $getter {
             Ok(ret) => ret,
             Err(_) => {
-                unsafe { *$err_no_p = Errno::EAGAIN as libc::c_int };
+                *$err_no_p = Errno::EAGAIN as libc::c_int;
                 return libc::c_int::from(NssStatus::TryAgain);
             }
         }
@@ -88,16 +88,17 @@ macro_rules! get_or_again {
 }
 
 #[no_mangle]
-pub extern "C" fn _nss_sectora_getpwnam_r(cnameptr: *const libc::c_char, pwptr: *mut Passwd, buf: *mut libc::c_char,
-                                          buflen: libc::size_t, errnop: *mut libc::c_int)
-                                          -> libc::c_int {
+pub unsafe extern "C" fn _nss_sectora_getpwnam_r(cnameptr: *const libc::c_char, pwptr: *mut Passwd,
+                                                 buf: *mut libc::c_char, buflen: libc::size_t,
+                                                 errnop: *mut libc::c_int)
+                                                 -> libc::c_int {
     let mut buffer = Buffer::new(buf, buflen);
     let name = string_from(cnameptr);
     let client = get_or_again!(Config::new(&CONF_PATH).and_then(|c| Ok(GithubClient::new(&c))), errnop);
     let sectors = get_or_again!(client.get_sectors(), errnop);
     for sector in sectors {
         if let Some(member) = sector.members.get(&name) {
-            match unsafe { (*pwptr).pack_args(&mut buffer, &member.login, member.id, sector.get_gid(), &client.conf) } {
+            match { (*pwptr).pack_args(&mut buffer, &member.login, member.id, sector.get_gid(), &client.conf) } {
                 Ok(_) => succeed!(),
                 Err(_) => fail!(errnop, Errno::ERANGE, NssStatus::TryAgain),
             }
@@ -107,18 +108,16 @@ pub extern "C" fn _nss_sectora_getpwnam_r(cnameptr: *const libc::c_char, pwptr: 
 }
 
 #[no_mangle]
-pub extern "C" fn _nss_sectora_getpwuid_r(uid: libc::uid_t, pwptr: *mut Passwd, buf: *mut libc::c_char,
-                                          buflen: libc::size_t, errnop: *mut libc::c_int)
-                                          -> libc::c_int {
+pub unsafe extern "C" fn _nss_sectora_getpwuid_r(uid: libc::uid_t, pwptr: *mut Passwd, buf: *mut libc::c_char,
+                                                 buflen: libc::size_t, errnop: *mut libc::c_int)
+                                                 -> libc::c_int {
     let mut buffer = Buffer::new(buf, buflen);
     let client = get_or_again!(Config::new(&CONF_PATH).and_then(|c| Ok(GithubClient::new(&c))), errnop);
     let sectors = get_or_again!(client.get_sectors(), errnop);
     for sector in sectors {
         for member in sector.members.values() {
             if uid == member.id as libc::uid_t {
-                match unsafe {
-                          (*pwptr).pack_args(&mut buffer, &member.login, member.id, sector.get_gid(), &client.conf)
-                      } {
+                match { (*pwptr).pack_args(&mut buffer, &member.login, member.id, sector.get_gid(), &client.conf) } {
                     Ok(_) => succeed!(),
                     Err(_) => fail!(errnop, Errno::ERANGE, NssStatus::TryAgain),
                 }
@@ -129,14 +128,14 @@ pub extern "C" fn _nss_sectora_getpwuid_r(uid: libc::uid_t, pwptr: *mut Passwd, 
 }
 
 #[no_mangle]
-pub extern "C" fn _nss_sectora_setpwent() -> libc::c_int {
+pub unsafe extern "C" fn _nss_sectora_setpwent() -> libc::c_int {
     let mut list_file = get_or_again!(runfiles::create());
     let client = get_or_again!(Config::new(&CONF_PATH).and_then(|c| Ok(GithubClient::new(&c))));
     let sectors = get_or_again!(client.get_sectors());
     for sector in sectors {
         for member in sector.members.values() {
             let mg = MemberGid { member: member.clone(),
-                                 gid: sector.get_gid(), };
+                                 gid: sector.get_gid() };
             list_file.write_all(mg.to_string().as_bytes()).unwrap();
         }
     }
@@ -144,15 +143,15 @@ pub extern "C" fn _nss_sectora_setpwent() -> libc::c_int {
 }
 
 #[no_mangle]
-pub extern "C" fn _nss_sectora_getpwent_r(pwptr: *mut Passwd, buf: *mut libc::c_char, buflen: libc::size_t,
-                                          errnop: *mut libc::c_int)
-                                          -> libc::c_int {
+pub unsafe extern "C" fn _nss_sectora_getpwent_r(pwptr: *mut Passwd, buf: *mut libc::c_char, buflen: libc::size_t,
+                                                 errnop: *mut libc::c_int)
+                                                 -> libc::c_int {
     let (idx, idx_file, list) = get_or_again!(runfiles::open(), errnop);
     let client = get_or_again!(Config::new(&CONF_PATH).and_then(|c| Ok(GithubClient::new(&c))), errnop);
     if let Some(Ok(line)) = list.lines().nth(idx) {
         let mut buffer = Buffer::new(buf, buflen);
         let mg = get_or_again!(line.parse::<MemberGid>(), errnop);
-        match unsafe { (*pwptr).pack_args(&mut buffer, &mg.member.login, mg.member.id, mg.gid, &client.conf) } {
+        match { (*pwptr).pack_args(&mut buffer, &mg.member.login, mg.member.id, mg.gid, &client.conf) } {
             Ok(_) => succeed!(runfiles::increment(idx, idx_file)),
             Err(_) => fail!(errnop, Errno::ERANGE, NssStatus::TryAgain),
         }
@@ -161,22 +160,23 @@ pub extern "C" fn _nss_sectora_getpwent_r(pwptr: *mut Passwd, buf: *mut libc::c_
 }
 
 #[no_mangle]
-pub extern "C" fn _nss_sectora_endpwent() -> libc::c_int {
+pub unsafe extern "C" fn _nss_sectora_endpwent() -> libc::c_int {
     runfiles::cleanup().unwrap_or(());
     libc::c_int::from(NssStatus::Success)
 }
 
 #[no_mangle]
-pub extern "C" fn _nss_sectora_getspnam_r(cnameptr: *const libc::c_char, spptr: *mut Spwd, buf: *mut libc::c_char,
-                                          buflen: libc::size_t, errnop: *mut libc::c_int)
-                                          -> libc::c_int {
+pub unsafe extern "C" fn _nss_sectora_getspnam_r(cnameptr: *const libc::c_char, spptr: *mut Spwd,
+                                                 buf: *mut libc::c_char, buflen: libc::size_t,
+                                                 errnop: *mut libc::c_int)
+                                                 -> libc::c_int {
     let mut buffer = Buffer::new(buf, buflen);
     let name = string_from(cnameptr);
     let client = get_or_again!(Config::new(&CONF_PATH).and_then(|c| Ok(GithubClient::new(&c))), errnop);
     let sectors = get_or_again!(client.get_sectors(), errnop);
     for sector in sectors {
         if let Some(member) = sector.members.get(&name) {
-            match unsafe { (*spptr).pack_args(&mut buffer, &member.login, &client.conf) } {
+            match { (*spptr).pack_args(&mut buffer, &member.login, &client.conf) } {
                 Ok(_) => succeed!(),
                 Err(_) => fail!(errnop, Errno::ERANGE, NssStatus::TryAgain),
             }
@@ -186,7 +186,7 @@ pub extern "C" fn _nss_sectora_getspnam_r(cnameptr: *const libc::c_char, spptr: 
 }
 
 #[no_mangle]
-pub extern "C" fn _nss_sectora_setspent() -> libc::c_int {
+pub unsafe extern "C" fn _nss_sectora_setspent() -> libc::c_int {
     let mut list_file = get_or_again!(runfiles::create());
     let client = get_or_again!(Config::new(&CONF_PATH).and_then(|c| Ok(GithubClient::new(&c))));
     let sectors = get_or_again!(client.get_sectors());
@@ -199,15 +199,15 @@ pub extern "C" fn _nss_sectora_setspent() -> libc::c_int {
 }
 
 #[no_mangle]
-pub extern "C" fn _nss_sectora_getspent_r(spptr: *mut Spwd, buf: *mut libc::c_char, buflen: libc::size_t,
-                                          errnop: *mut libc::c_int)
-                                          -> libc::c_int {
+pub unsafe extern "C" fn _nss_sectora_getspent_r(spptr: *mut Spwd, buf: *mut libc::c_char, buflen: libc::size_t,
+                                                 errnop: *mut libc::c_int)
+                                                 -> libc::c_int {
     let (idx, idx_file, list) = get_or_again!(runfiles::open(), errnop);
     let client = get_or_again!(Config::new(&CONF_PATH).and_then(|c| Ok(GithubClient::new(&c))), errnop);
     if let Some(Ok(line)) = list.lines().nth(idx) {
         let mut buffer = Buffer::new(buf, buflen);
         let member = get_or_again!(line.parse::<Member>(), errnop);
-        match unsafe { (*spptr).pack_args(&mut buffer, &member.login, &client.conf) } {
+        match { (*spptr).pack_args(&mut buffer, &member.login, &client.conf) } {
             Ok(_) => succeed!(runfiles::increment(idx, idx_file)),
             Err(_) => fail!(errnop, Errno::ERANGE, NssStatus::TryAgain),
         }
@@ -216,22 +216,22 @@ pub extern "C" fn _nss_sectora_getspent_r(spptr: *mut Spwd, buf: *mut libc::c_ch
 }
 
 #[no_mangle]
-pub extern "C" fn _nss_sectora_endspent() -> libc::c_int {
+pub unsafe extern "C" fn _nss_sectora_endspent() -> libc::c_int {
     runfiles::cleanup().unwrap_or(());
     libc::c_int::from(NssStatus::Success)
 }
 
 #[no_mangle]
-pub extern "C" fn _nss_sectora_getgrgid_r(gid: libc::gid_t, grptr: *mut Group, buf: *mut libc::c_char,
-                                          buflen: libc::size_t, errnop: *mut libc::c_int)
-                                          -> libc::c_int {
+pub unsafe extern "C" fn _nss_sectora_getgrgid_r(gid: libc::gid_t, grptr: *mut Group, buf: *mut libc::c_char,
+                                                 buflen: libc::size_t, errnop: *mut libc::c_int)
+                                                 -> libc::c_int {
     let mut buffer = Buffer::new(buf, buflen);
     let client = get_or_again!(Config::new(&CONF_PATH).and_then(|c| Ok(GithubClient::new(&c))), errnop);
     let sectors = get_or_again!(client.get_sectors(), errnop);
     for sector in sectors {
         let members: Vec<&str> = sector.members.values().map(|m| m.login.as_str()).collect();
         if gid as u64 == sector.get_gid() {
-            match unsafe { (*grptr).pack_args(&mut buffer, &sector.get_group(), gid as u64, &members) } {
+            match { (*grptr).pack_args(&mut buffer, &sector.get_group(), gid as u64, &members) } {
                 Ok(_) => succeed!(),
                 Err(_) => fail!(errnop, Errno::ERANGE, NssStatus::TryAgain),
             }
@@ -241,9 +241,10 @@ pub extern "C" fn _nss_sectora_getgrgid_r(gid: libc::gid_t, grptr: *mut Group, b
 }
 
 #[no_mangle]
-pub extern "C" fn _nss_sectora_getgrnam_r(cnameptr: *const libc::c_char, grptr: *mut Group, buf: *mut libc::c_char,
-                                          buflen: libc::size_t, errnop: *mut libc::c_int)
-                                          -> libc::c_int {
+pub unsafe extern "C" fn _nss_sectora_getgrnam_r(cnameptr: *const libc::c_char, grptr: *mut Group,
+                                                 buf: *mut libc::c_char, buflen: libc::size_t,
+                                                 errnop: *mut libc::c_int)
+                                                 -> libc::c_int {
     let mut buffer = Buffer::new(buf, buflen);
     let name = string_from(cnameptr);
     let client = get_or_again!(Config::new(&CONF_PATH).and_then(|c| Ok(GithubClient::new(&c))), errnop);
@@ -251,7 +252,7 @@ pub extern "C" fn _nss_sectora_getgrnam_r(cnameptr: *const libc::c_char, grptr: 
     for sector in sectors {
         let members: Vec<&str> = sector.members.values().map(|m| m.login.as_str()).collect();
         if name == sector.get_group() {
-            match unsafe { (*grptr).pack_args(&mut buffer, &sector.get_group(), sector.get_gid(), &members) } {
+            match { (*grptr).pack_args(&mut buffer, &sector.get_group(), sector.get_gid(), &members) } {
                 Ok(_) => succeed!(),
                 Err(_) => fail!(errnop, Errno::ERANGE, NssStatus::TryAgain),
             }
@@ -261,7 +262,7 @@ pub extern "C" fn _nss_sectora_getgrnam_r(cnameptr: *const libc::c_char, grptr: 
 }
 
 #[no_mangle]
-pub extern "C" fn _nss_sectora_setgrent() -> libc::c_int {
+pub unsafe extern "C" fn _nss_sectora_setgrent() -> libc::c_int {
     let mut list_file = get_or_again!(runfiles::create());
     let client = get_or_again!(Config::new(&CONF_PATH).and_then(|c| Ok(GithubClient::new(&c))));
     let sectors = get_or_again!(client.get_sectors());
@@ -272,15 +273,15 @@ pub extern "C" fn _nss_sectora_setgrent() -> libc::c_int {
 }
 
 #[no_mangle]
-pub extern "C" fn _nss_sectora_getgrent_r(grptr: *mut Group, buf: *mut libc::c_char, buflen: libc::size_t,
-                                          errnop: *mut libc::c_int)
-                                          -> libc::c_int {
+pub unsafe extern "C" fn _nss_sectora_getgrent_r(grptr: *mut Group, buf: *mut libc::c_char, buflen: libc::size_t,
+                                                 errnop: *mut libc::c_int)
+                                                 -> libc::c_int {
     let (idx, idx_file, list) = get_or_again!(runfiles::open(), errnop);
     if let Some(Ok(line)) = list.lines().nth(idx) {
         let mut buffer = Buffer::new(buf, buflen);
         let sector = get_or_again!(line.parse::<SectorGroup>(), errnop);
-        match unsafe { (*grptr).pack_args(&mut buffer, &sector.get_group(), sector.get_gid(), &members) } {
         let members: Vec<&str> = sector.members.keys().map(|k| k.as_str()).collect();
+        match { (*grptr).pack_args(&mut buffer, &sector.get_group(), sector.get_gid(), &members) } {
             Ok(_) => succeed!(runfiles::increment(idx, idx_file)),
             Err(_) => fail!(errnop, Errno::ERANGE, NssStatus::TryAgain),
         }
@@ -289,7 +290,7 @@ pub extern "C" fn _nss_sectora_getgrent_r(grptr: *mut Group, buf: *mut libc::c_c
 }
 
 #[no_mangle]
-pub extern "C" fn _nss_sectora_endgrent() -> libc::c_int {
+pub unsafe extern "C" fn _nss_sectora_endgrent() -> libc::c_int {
     runfiles::cleanup().unwrap_or(());
     libc::c_int::from(NssStatus::Success)
 }
