0641d226c5f7590d1d879281fe8d051b687bd51e
yasuyuky <yasuyuki.ymd@gmail.com>
2022-12-31 16:49:49 +0900

    Use once_cell instead of lazy_static

--- src/statics.rs
+++ src/statics.rs
@@ -1,11 +1,10 @@
+use once_cell::sync::Lazy;
 use std::env;
-use std::path::Path;
+use std::path::PathBuf;
 use std::string::String;
-use lazy_static::lazy_static;
 
 const DEFAULT_CONF_PATH_STR: &str = "/etc/sectora.conf";
 
-lazy_static! {
-    static ref CONF_PATH_STR: String = env::var("SECTORA_CONFIG").unwrap_or(String::from(DEFAULT_CONF_PATH_STR));
-    pub static ref CONF_PATH: &'static Path = Path::new(&(*CONF_PATH_STR));
-}
+static CONF_PATH_STR: Lazy<String> =
+    Lazy::new(|| env::var("SECTORA_CONFIG").unwrap_or(String::from(DEFAULT_CONF_PATH_STR)));
+pub static CONF_PATH: Lazy<PathBuf> = Lazy::new(|| PathBuf::from((*CONF_PATH_STR).clone()));
